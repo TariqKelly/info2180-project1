63ec4f49d9e08785abbfd007bdba01a02010745c
Tariq Kelly <tariqkelly@gmail.com>
2021-10-14 19:36:47 -0500

    Added functionality for newsletter form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-/* Add your JavaScript to this file */
+window.onload = () => {
+    var message = document.getElementsByClassName("message")[0];
+    var input = document.getElementsByTagName("input")[0];
+    var submitBtn = document.getElementsByTagName("button")[0];
+
+    submitBtn.setAttribute("type","button");
+    submitBtn.addEventListener("click", () => {
+        if (input.value.length == 0) {
+            message.innerHTML = "Please enter a valid email address";
+        }
+        else {
+            message.innerHTML = `Thank you! Your email address ${input.value} has been added to our mailing list!`;
+        } 
+})
+    
+}
+
+
+
+
+
+    
+
+
+        
